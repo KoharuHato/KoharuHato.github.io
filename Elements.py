0cccb997f3560a3e757af76e812008e301612152
Koharu <154706906+KoharuHato@users.noreply.github.com>
2024-03-26 18:47:43 -0400

    Update Elements.py

--- Elements.py
+++ Elements.py
@@ -1,3 +1,9 @@
+<!DOCTYPE python>
+<html lang = "en">
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/3/w3.css">
+
 <!-- Navigation -->
 <nav class="w3-bar w3-grey">
     <a href="#index.html" class="w3-button w3-bar-item">Home</a>
