7941d01369a4875cd0e990cab8281b5547577ef6
KoharuHato <154706906+KoharuHato@users.noreply.github.com>
2024-09-03 09:49:16 -0400

    Update script.js

--- script.js
+++ script.js
@@ -89,7 +89,7 @@ cells.forEach((cell, index) => {
 document.addEventListener('DOMContentLoaded', () => {
     const products = [
         { id: 1, name: 'Product 1', price: 10.00 },
-        { id: 2, name: 'Product 2', price: 15.00 },
+        { id: 2, name: 'Boba', price: 15.00 },
         { id: 3, name: 'Product 3', price: 20.00 },
     ];
 
